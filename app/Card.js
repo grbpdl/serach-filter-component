8d931a13bec6a0d29e6b0850a67056bb669df372
Gaurab Paudyal <gaurab.191324@ncit.edu.np>
2024-08-28 08:46:19 +0545

    made the basic UI for the search and filters

---
+++ app/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='w-auto h-100 bg-white m-2 p-1 text-black rounded'>
+          <p>Name of Product</p>
+          <p>Price:89999</p>
+          <p>Rating:4</p>
+        </div>
+  )
+}
+
+export default Card
---
+++ app/details.js
@@ -0,0 +1,502 @@
+export const data=[{
+    "id": 1,
+    "product_name": "Sauce - Cranberry",
+    "price": 7786,
+    "rating": 2
+  }, {
+    "id": 2,
+    "product_name": "Nantucket Apple Juice",
+    "price": 8362,
+    "rating": 0
+  }, {
+    "id": 3,
+    "product_name": "Pasta - Agnolotti - Butternut",
+    "price": 6248,
+    "rating": 4
+  }, {
+    "id": 4,
+    "product_name": "Salt And Pepper Mix - White",
+    "price": 4269,
+    "rating": 4
+  }, {
+    "id": 5,
+    "product_name": "Lid Coffee Cup 8oz Blk",
+    "price": 3756,
+    "rating": 4
+  }, {
+    "id": 6,
+    "product_name": "Oregano - Fresh",
+    "price": 8495,
+    "rating": 2
+  }, {
+    "id": 7,
+    "product_name": "Beer - Creemore",
+    "price": 4745,
+    "rating": 4
+  }, {
+    "id": 8,
+    "product_name": "Praline Paste",
+    "price": 7754,
+    "rating": 4
+  }, {
+    "id": 9,
+    "product_name": "Coconut - Shredded, Unsweet",
+    "price": 7657,
+    "rating": 1
+  }, {
+    "id": 10,
+    "product_name": "Bread Cranberry Foccacia",
+    "price": 1650,
+    "rating": 1
+  }, {
+    "id": 11,
+    "product_name": "Wine - Baron De Rothschild",
+    "price": 3370,
+    "rating": 1
+  }, {
+    "id": 12,
+    "product_name": "Muffin - Mix - Strawberry Rhubarb",
+    "price": 4623,
+    "rating": 0
+  }, {
+    "id": 13,
+    "product_name": "Chicken Giblets",
+    "price": 5880,
+    "rating": 3
+  }, {
+    "id": 14,
+    "product_name": "Veal - Osso Bucco",
+    "price": 6679,
+    "rating": 0
+  }, {
+    "id": 15,
+    "product_name": "Flour - Semolina",
+    "price": 1242,
+    "rating": 5
+  }, {
+    "id": 16,
+    "product_name": "Bananas",
+    "price": 4568,
+    "rating": 5
+  }, {
+    "id": 17,
+    "product_name": "Wine - Charddonnay Errazuriz",
+    "price": 2187,
+    "rating": 3
+  }, {
+    "id": 18,
+    "product_name": "Lettuce - Arugula",
+    "price": 5314,
+    "rating": 0
+  }, {
+    "id": 19,
+    "product_name": "Wine - Magnotta - Bel Paese White",
+    "price": 1192,
+    "rating": 1
+  }, {
+    "id": 20,
+    "product_name": "Carbonated Water - Orange",
+    "price": 2742,
+    "rating": 4
+  }, {
+    "id": 21,
+    "product_name": "Appetizer - Assorted Box",
+    "price": 1442,
+    "rating": 0
+  }, {
+    "id": 22,
+    "product_name": "Beef - Top Sirloin",
+    "price": 8087,
+    "rating": 3
+  }, {
+    "id": 23,
+    "product_name": "Snapple - Mango Maddness",
+    "price": 7197,
+    "rating": 3
+  }, {
+    "id": 24,
+    "product_name": "Truffle Shells - Semi - Sweet",
+    "price": 7733,
+    "rating": 5
+  }, {
+    "id": 25,
+    "product_name": "Wine - Rosso Del Veronese Igt",
+    "price": 8106,
+    "rating": 0
+  }, {
+    "id": 26,
+    "product_name": "Samosa - Veg",
+    "price": 1631,
+    "rating": 4
+  }, {
+    "id": 27,
+    "product_name": "Black Currants",
+    "price": 1644,
+    "rating": 2
+  }, {
+    "id": 28,
+    "product_name": "Crackers - Melba Toast",
+    "price": 6518,
+    "rating": 1
+  }, {
+    "id": 29,
+    "product_name": "Mushroom - Oyster, Fresh",
+    "price": 2074,
+    "rating": 3
+  }, {
+    "id": 30,
+    "product_name": "Beans - Butter Lrg Lima",
+    "price": 7335,
+    "rating": 2
+  }, {
+    "id": 31,
+    "product_name": "Pepper - Cubanelle",
+    "price": 925,
+    "rating": 3
+  }, {
+    "id": 32,
+    "product_name": "Wine - Acient Coast Caberne",
+    "price": 3969,
+    "rating": 2
+  }, {
+    "id": 33,
+    "product_name": "Grenadine",
+    "price": 3540,
+    "rating": 4
+  }, {
+    "id": 34,
+    "product_name": "Table Cloth 81x81 White",
+    "price": 3575,
+    "rating": 5
+  }, {
+    "id": 35,
+    "product_name": "Oil - Sesame",
+    "price": 3761,
+    "rating": 3
+  }, {
+    "id": 36,
+    "product_name": "The Pop Shoppe - Lime Rickey",
+    "price": 2712,
+    "rating": 0
+  }, {
+    "id": 37,
+    "product_name": "Cookies - Fortune",
+    "price": 6882,
+    "rating": 4
+  }, {
+    "id": 38,
+    "product_name": "Pastry - Mini French Pastries",
+    "price": 6026,
+    "rating": 1
+  }, {
+    "id": 39,
+    "product_name": "Prunes - Pitted",
+    "price": 6362,
+    "rating": 2
+  }, {
+    "id": 40,
+    "product_name": "Pail With Metal Handle 16l White",
+    "price": 8480,
+    "rating": 3
+  }, {
+    "id": 41,
+    "product_name": "Galliano",
+    "price": 3469,
+    "rating": 0
+  }, {
+    "id": 42,
+    "product_name": "Beef - Short Ribs",
+    "price": 6178,
+    "rating": 5
+  }, {
+    "id": 43,
+    "product_name": "Bread - Focaccia Quarter",
+    "price": 9064,
+    "rating": 0
+  }, {
+    "id": 44,
+    "product_name": "Soup - Campbells Asian Noodle",
+    "price": 3989,
+    "rating": 1
+  }, {
+    "id": 45,
+    "product_name": "Chevere Logs",
+    "price": 6528,
+    "rating": 2
+  }, {
+    "id": 46,
+    "product_name": "Steamers White",
+    "price": 3951,
+    "rating": 2
+  }, {
+    "id": 47,
+    "product_name": "Nantucket Apple Juice",
+    "price": 5453,
+    "rating": 1
+  }, {
+    "id": 48,
+    "product_name": "Muffin Mix - Carrot",
+    "price": 1835,
+    "rating": 4
+  }, {
+    "id": 49,
+    "product_name": "Lobster - Live",
+    "price": 1849,
+    "rating": 0
+  }, {
+    "id": 50,
+    "product_name": "Bread - Calabrese Baguette",
+    "price": 7590,
+    "rating": 2
+  }, {
+    "id": 51,
+    "product_name": "Laundry - Bag Cloth",
+    "price": 4014,
+    "rating": 4
+  }, {
+    "id": 52,
+    "product_name": "Beef - Kobe Striploin",
+    "price": 9602,
+    "rating": 5
+  }, {
+    "id": 53,
+    "product_name": "Tart - Pecan Butter Squares",
+    "price": 745,
+    "rating": 1
+  }, {
+    "id": 54,
+    "product_name": "Jam - Apricot",
+    "price": 5577,
+    "rating": 4
+  }, {
+    "id": 55,
+    "product_name": "Cumin - Ground",
+    "price": 2682,
+    "rating": 0
+  }, {
+    "id": 56,
+    "product_name": "Beer - Sleemans Cream Ale",
+    "price": 3452,
+    "rating": 4
+  }, {
+    "id": 57,
+    "product_name": "Bread - White Epi Baguette",
+    "price": 1154,
+    "rating": 2
+  }, {
+    "id": 58,
+    "product_name": "Sobe - Liz Blizz",
+    "price": 3051,
+    "rating": 4
+  }, {
+    "id": 59,
+    "product_name": "Bread - Roll, Soft White Round",
+    "price": 9516,
+    "rating": 0
+  }, {
+    "id": 60,
+    "product_name": "Coffee Guatemala Dark",
+    "price": 8006,
+    "rating": 4
+  }, {
+    "id": 61,
+    "product_name": "V8 Pet",
+    "price": 1641,
+    "rating": 3
+  }, {
+    "id": 62,
+    "product_name": "Pepper - Paprika, Spanish",
+    "price": 1680,
+    "rating": 4
+  }, {
+    "id": 63,
+    "product_name": "Beets - Mini Golden",
+    "price": 3024,
+    "rating": 2
+  }, {
+    "id": 64,
+    "product_name": "Lemons",
+    "price": 5542,
+    "rating": 2
+  }, {
+    "id": 65,
+    "product_name": "Table Cloth 53x69 White",
+    "price": 1139,
+    "rating": 5
+  }, {
+    "id": 66,
+    "product_name": "Lettuce - Green Leaf",
+    "price": 4866,
+    "rating": 5
+  }, {
+    "id": 67,
+    "product_name": "Lid Coffeecup 12oz D9542b",
+    "price": 8485,
+    "rating": 0
+  }, {
+    "id": 68,
+    "product_name": "Puree - Blackcurrant",
+    "price": 6524,
+    "rating": 4
+  }, {
+    "id": 69,
+    "product_name": "Squid U5 - Thailand",
+    "price": 7150,
+    "rating": 4
+  }, {
+    "id": 70,
+    "product_name": "Pasta - Fusili Tri - Coloured",
+    "price": 6536,
+    "rating": 4
+  }, {
+    "id": 71,
+    "product_name": "Wine - George Duboeuf Rose",
+    "price": 7635,
+    "rating": 4
+  }, {
+    "id": 72,
+    "product_name": "Coke - Classic, 355 Ml",
+    "price": 4634,
+    "rating": 1
+  }, {
+    "id": 73,
+    "product_name": "Olives - Stuffed",
+    "price": 9001,
+    "rating": 4
+  }, {
+    "id": 74,
+    "product_name": "Sea Bass - Fillets",
+    "price": 7350,
+    "rating": 5
+  }, {
+    "id": 75,
+    "product_name": "Sugar - Fine",
+    "price": 7774,
+    "rating": 5
+  }, {
+    "id": 76,
+    "product_name": "Eggplant Italian",
+    "price": 1031,
+    "rating": 5
+  }, {
+    "id": 77,
+    "product_name": "Flour - Bread",
+    "price": 8370,
+    "rating": 1
+  }, {
+    "id": 78,
+    "product_name": "Rootbeer",
+    "price": 3381,
+    "rating": 3
+  }, {
+    "id": 79,
+    "product_name": "Lettuce - Spring Mix",
+    "price": 2613,
+    "rating": 1
+  }, {
+    "id": 80,
+    "product_name": "Beer - Camerons Auburn",
+    "price": 9526,
+    "rating": 1
+  }, {
+    "id": 81,
+    "product_name": "Carbonated Water - White Grape",
+    "price": 4372,
+    "rating": 0
+  }, {
+    "id": 82,
+    "product_name": "Beans - Wax",
+    "price": 584,
+    "rating": 2
+  }, {
+    "id": 83,
+    "product_name": "Ice Cream - Chocolate",
+    "price": 5229,
+    "rating": 1
+  }, {
+    "id": 84,
+    "product_name": "Cookie Dough - Double",
+    "price": 1395,
+    "rating": 2
+  }, {
+    "id": 85,
+    "product_name": "Potato - Sweet",
+    "price": 7161,
+    "rating": 5
+  }, {
+    "id": 86,
+    "product_name": "Bagelers - Cinn / Brown Sugar",
+    "price": 7661,
+    "rating": 5
+  }, {
+    "id": 87,
+    "product_name": "Plastic Arrow Stir Stick",
+    "price": 3458,
+    "rating": 3
+  }, {
+    "id": 88,
+    "product_name": "Dragon Fruit",
+    "price": 9591,
+    "rating": 3
+  }, {
+    "id": 89,
+    "product_name": "Cake - Mini Cheesecake",
+    "price": 8863,
+    "rating": 2
+  }, {
+    "id": 90,
+    "product_name": "Pepper - Black, Whole",
+    "price": 6053,
+    "rating": 3
+  }, {
+    "id": 91,
+    "product_name": "Soup - Campbells Chili",
+    "price": 949,
+    "rating": 4
+  }, {
+    "id": 92,
+    "product_name": "Salmon - Atlantic, Fresh, Whole",
+    "price": 3706,
+    "rating": 0
+  }, {
+    "id": 93,
+    "product_name": "Bacardi Breezer - Tropical",
+    "price": 955,
+    "rating": 0
+  }, {
+    "id": 94,
+    "product_name": "Mace",
+    "price": 2887,
+    "rating": 3
+  }, {
+    "id": 95,
+    "product_name": "Bread - Multigrain",
+    "price": 7941,
+    "rating": 5
+  }, {
+    "id": 96,
+    "product_name": "Yogurt - Banana, 175 Gr",
+    "price": 519,
+    "rating": 5
+  }, {
+    "id": 97,
+    "product_name": "Pork - European Side Bacon",
+    "price": 6702,
+    "rating": 0
+  }, {
+    "id": 98,
+    "product_name": "Soupcontfoam16oz 116con",
+    "price": 536,
+    "rating": 4
+  }, {
+    "id": 99,
+    "product_name": "Bread - Raisin Walnut Oval",
+    "price": 2604,
+    "rating": 1
+  }, {
+    "id": 100,
+    "product_name": "Venison - Striploin",
+    "price": 9361,
+    "rating": 3
+  }]
+  
